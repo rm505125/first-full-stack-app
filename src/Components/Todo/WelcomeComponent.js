ebfa9d9de5702daf04ea0febd82ddbb644bdd4b8
Rahul Maurya <rm505125@gmail.com>
2022-04-15 17:58:46 +0530

    port changed

--- src/Components/Todo/WelcomeComponent.js
+++ src/Components/Todo/WelcomeComponent.js
@@ -1,6 +1,11 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
 class WelcomeComponent extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {};
+  // }
   render() {
     return (
       <>
@@ -9,8 +14,21 @@ class WelcomeComponent extends Component {
           Welcome {this.props.params.name}.You can manage your todos{" "}
           <Link to="/todos">here</Link>
         </div>
+
+        <div>
+          Click here to get a customized welcome message.
+          <button
+            onClick={this.retrieveWelcomeMessage}
+            className="btn btn-success"
+          >
+            Get Welcome
+          </button>
+        </div>
       </>
     );
   }
+  retrieveWelcomeMessage = () => {
+    console.log("retrieve clicked!");
+  };
 }
 export default WelcomeComponent;
